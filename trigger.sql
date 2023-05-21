55659fae096c1dba370ced2c0879feace2726a9b
Kevin <105234150+Kevin-qt@users.noreply.github.com>
2023-05-21 00:33:00 -0400

    Add trigger file
    The file contains trigger defined in this database.

---
+++ trigger.sql
@@ -0,0 +1,18 @@
+DELIMITER //
+
+CREATE TRIGGER update_balance
+AFTER INSERT ON transactions
+FOR EACH ROW
+BEGIN
+    IF NEW.CategoryID = 1 THEN
+        UPDATE accounts
+        SET accounts.Balance = accounts.Balance + NEW.Amount
+        WHERE accounts.UserID = NEW.UserID;
+	ELSE
+		UPDATE accounts
+        SET accounts.Balance = accounts.Balance - NEW.Amount
+        WHERE accounts.UserID = NEW.UserID;
+    END IF;
+END; //
+
+DELIMITER ;
